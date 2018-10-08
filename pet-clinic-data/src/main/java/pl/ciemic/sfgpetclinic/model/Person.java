9526a51bf393b209bc047c03609a9b8890fc485e
ciemic <michal.cieslar@gmail.com>
2018-10-08 17:07:24 +0200

    adding base entity. closes #10

--- pet-clinic-data/src/main/java/pl/ciemic/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/pl/ciemic/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package pl.ciemic.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/pl/ciemic/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/pl/ciemic/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package pl.ciemic.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
