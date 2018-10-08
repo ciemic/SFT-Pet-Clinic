de1421dc351394ca33483e5d9126ebde36d880df
ciemic <michal.cieslar@gmail.com>
2018-10-08 22:02:17 +0200

    Refactor to common crud interface. closes #11

--- pet-clinic-data/src/main/java/pl/ciemic/sfgpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/pl/ciemic/sfgpetclinic/services/VetService.java
@@ -2,12 +2,6 @@ package pl.ciemic.sfgpetclinic.services;
 
 import pl.ciemic.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
